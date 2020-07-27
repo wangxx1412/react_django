775b31608fea940362a26dfaa0c7c74bcb295388
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-07-27 16:00:37 -0700

    Add leads table and update delete lead

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS } from "./types";
+import { GET_LEADS, DELETE_LEADS } from "./types";
 
 // Get leads
 export const getLeads = () => (dispatch) => {
@@ -8,6 +8,16 @@ export const getLeads = () => (dispatch) => {
     .get("/api/leads/")
     .then((res) => {
       dispatch({ type: GET_LEADS, payload: res.data });
+    })
+    .catch((err) => console.log(err));
+};
+
+// Delete leads
+export const deleteLeads = () => (dispatch) => {
+  axios
+    .get(`/api/leads/${id}`)
+    .then((res) => {
+      dispatch({ type: DELETE_LEADS, payload: id });
     })
     .catch((err) => console.log(err));
 };
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { getLeads } from "../../actions/leads";
+import { getLeads, deleteLeads } from "../../actions/leads";
+import leads from "../../reducers/leads";
 
 export class Leads extends Component {
   static propTypes = {
@@ -14,9 +15,38 @@ export class Leads extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Leads</h1>
-      </div>
+      <Fragment>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map((lead) => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button
+                    onClick={this.props.deleteLeads.bind(this, lead.id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     );
   }
 }
